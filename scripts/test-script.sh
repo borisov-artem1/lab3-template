7b5d47d9768a02de9cedf7257058e274b62fad5e
Artem Borisov <artem.borisov2002@mail.ru>
2024-12-11 20:52:07 +0300

    done

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -36,7 +36,7 @@ step() {
 
   printf "=== Step %d: %s %s ===\n" "$step" "$operation" "$service"
 
-  docker compose "$operation" "$service"
+  docker-compose "$operation" "$service"
   if [[ "$operation" == "start" ]]; then
     "$path"/wait-for.sh -t 120 "http://84.252.133.253:$port/api/v1/manage/health" -- echo "Host localhost:$port is active"
     sleep 10
